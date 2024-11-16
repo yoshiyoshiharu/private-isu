326382513e8427f33445462cf10349383ae85d9c
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-11-16 16:54:42 +0900

    setting stackprof

--- ruby/app.rb
+++ ruby/app.rb
@@ -13,6 +13,20 @@ module Isuconp
     UPLOAD_LIMIT = 10 * 1024 * 1024 # 10mb
 
     POSTS_PER_PAGE = 20
+    before do
+      StackProf.start(
+        enabled: true,
+        mode: :cpu,
+        raw: true,
+        interval: 1000,
+        save_every: 5
+      )
+     end
+
+     after do
+       StackProf.stop
+       StackProf.results("../measure/ruby/stackprof.dump")
+     end
 
     helpers do
       def config

ced543e32e16819ce314b6f88f5af8d291218053
Taisei Sato <work10tai13prog@gmail.com>
2024-11-16 17:02:31 +0900

    feat: etcディレクトリにミドルウェアの設定ファイルをコピーするスクリプトを追加

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,6 +4,7 @@ set -e
 
 USERS=("yoshiyoshiharu" "KoyamaShimpei" "taiwork")
 REPOSITORY_NAME="git@github.com:yoshiyoshiharu/isucon14-qualify.git"
+GIT_REPOSITORY_DIR="$HOME"
 
 # authorized_keysにgithubに登録しているssh公開鍵を登録する
 for user in ${USERS[@]}; do
@@ -14,3 +15,16 @@ echo "authorized_keysにGithubのSSH公開鍵を登録しました"
 git config --global core.editor "vim"
 git config --global user.email "haruki.osaka.u@gmail.com"
 git config --global user.name "yoshiyoshiharu"
+
+# etc配下のミドルウェアの設定ファイルをホームディレクリにコピー
+echo "start: ミドルウェアの設定ファイルをホームディレクトリにコピーします"
+NGINX_CONF_DIR="/etc/nginx"
+MYSQL_CONF_DIR="/etc/mysql"
+WHOAMI="isucon" 
+
+mkdir -p ${GIT_REPOSITORY_DIR}/etc
+
+cp -r ${NGINX_CONF_DIR} ${GIT_REPOSITORY_DIR}/etc
+cp -r ${MYSQL_CONF_DIR} ${GIT_REPOSITORY_DIR}/etc
+chown -R ${WHOAMI}:${WHOAMI} ${GIT_REPOSITORY_DIR}/etc
+echo "done: ミドルウェアの設定ファイルをホームディレクトリにコピーしました"

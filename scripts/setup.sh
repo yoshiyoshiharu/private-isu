3972f777358841d77d22b316988f7302b5e49c3c
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-11-16 16:37:29 +0900

    fix script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,7 +7,7 @@ REPOSITORY_NAME="git@github.com:yoshiyoshiharu/isucon14-qualify.git"
 
 # authorized_keysにgithubに登録しているssh公開鍵を登録する
 for user in ${USERS[@]}; do
-  echo $(curl -fs "https://github.com/${user}.keys") >> ~/.ssh/authorized_keys
+  curl -fs "https://github.com/${user}.keys" >> ~/.ssh/authorized_keys
 done
 echo "authorized_keysにGithubのSSH公開鍵を登録しました"
 

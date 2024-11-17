75afe41b3cdf06b3a2f060e16d98a5eb3e6b77f1
Haruki Yoshida <haruki.osaka.u@gmail.com>
2024-11-17 22:10:04 +0900

    Fix stackprof (#16)
    * fix
    
    * delete stackprof.dunpm
    
    * measure with rack
    
    * fix

--- ruby/config.ru
+++ ruby/config.ru
@@ -13,5 +13,8 @@ class DropContentTypeFromGet
 end
 
 use DropContentTypeFromGet
-
+use StackProf::Middleware, enabled: true,
+                           mode: :cpu,
+                           interval: 1000,
+                           save_every: 5
 run Isuconp::App
--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -6,7 +6,7 @@ GIT_REPOSITORY_DIR="$HOME/private_isu/webapp"
 SYSTEMCTL_APP="isu-ruby"
 MYSQL_SLOW_LOG="/var/log/mysql/mysql-slow.log"
 NGINX_ACCESS_LOG="/var/log/nginx/access.log"
-APP_PROF="$GIT_REPOSITORY_DIR/measure/ruby/stackprof.dump"
+APP_PROF="$GIT_REPOSITORY_DIR/ruby/tmp"
 
 # 最新状態にする
 git pull
@@ -18,7 +18,9 @@ cp -r ${GIT_REPOSITORY_DIR}/etc/mysql/* /etc/mysql
 # ログをクリア
 sudo truncate -s 0 "${MYSQL_SLOW_LOG}"
 sudo truncate -s 0 "${NGINX_ACCESS_LOG}"
-sudo truncate -s 0 "${APP_PROF}"
+if [ -d $APP_HOME ]; then
+  rm -r ${APP_PROF}
+fi
 
 # サービスの再起動 最初に設定する
 sudo systemctl restart nginx

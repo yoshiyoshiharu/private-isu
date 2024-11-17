4e85e2899eaab27041018f724ce1b91bc6205b4a
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-11-17 12:13:57 +0900

    fix resukt

--- scripts/result.sh
+++ scripts/result.sh
@@ -1,13 +1,15 @@
 #!/bin/sh
-HOME=/home/isucon
-APP_HOME=${HOME}/webapp
+
+# [TODO: 最初に設定] 環境変数
+APP_HOME=~/private_isu/webapp
 
 MYSQL_SLOW_LOG="/var/log/mysql/slow.log"
 NGINX_ACCESS_LOG="/var/log/nginx/access.log"
 NGINX_ACCESS_LOG_FORMAT="${APP_HOME}/measure/nginx/access.log"
 
 ALPSORT=sum
-# パスパターンを配列として定義
+
+# [TODO: API見ながら設定] パスパターンを配列として定義
 ALP_PATTERNS=(
     "/api/user/.+/icon"
     "/api/user/.+/theme"
@@ -28,7 +30,6 @@ ALP_PATTERNS=(
 ALPM=$(IFS=,; echo "${ALP_PATTERNS[*]}")
 OUTFORMAT=count,method,uri,min,max,sum,avg,p99
 
-
 # alp
 sudo alp ltsv --file=${NGINX_ACCESS_LOG} --nosave-pos --pos /tmp/alp.pos --sort ${ALPSORT} --reverse -o ${OUTFORMAT} -q -m ${ALPM} > ${NGINX_ACCESS_LOG_FORMAT}
 echo "Nginxのログフォーマット完了"
@@ -39,6 +40,6 @@ stackprof --d3-flamegraph ${APP_HOME}/measure/ruby/stackprof.dump > ${APP_HOME}/
 echo "stackprofのhtmlへの変換完了"
 
 # mysql
-ssh 57.180.240.214 sudo mysqldumpslow -s t /var/log/mysql/mysql-slow.log > ${APP_HOME}/measure/mysql/mysql-slow.log
+sudo mysqldumpslow -s t /var/log/mysql/mysql-slow.log > ${APP_HOME}/measure/mysql/mysql-slow.log
 echo "Mysqlのスロークエリ出力完了"
 

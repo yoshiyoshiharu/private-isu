a7e67e2fd42f036ec66e668a6d9dcd234b2746b5
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-11-18 00:24:47 +0900

    fix

--- sql/index.sql
+++ sql/index.sql
@@ -1,4 +1,4 @@
-ALTER TABLE posts ADD COLUMN comments_count integer DEFAULT 0;
+ALTER TABLE posts ADD COLUMN comment_count integer DEFAULT 0;
 CREATE INDEX index_comments_post_id_created_at ON comments (post_id, created_at);
 CREATE INDEX index_comments_user_id ON comments (user_id);
 CREATE INDEX index_posts_user_id_created_at ON posts (user_id, created_at);

fbbd0f6378250a141d885b5c989eb66c1e035d3f
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-11-17 14:23:38 +0900

    add index

---
+++ sql/index.sql
@@ -0,0 +1,4 @@
+CREATE INDEX index_comments_post_id_created_at ON comments (post_id, created_at);
+CREATE INDEX index_comments_user_id ON comments (user_id);
+CREATE INDEX index_posts_user_id_created_at ON posts (user_id, created_at);
+CREATE INDEX index_posts_created_at ON posts (created_at);

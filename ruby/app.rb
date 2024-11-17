44ef3b8db5a32183dcbd6be5a8ffcc5cedf427ae
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-11-17 17:51:14 +0900

    fix typo

--- ruby/app.rb
+++ ruby/app.rb
@@ -365,7 +365,7 @@ module Isuconp
 
       post = db.prepare('SELECT * FROM `posts` WHERE `id` = ?').execute(params[:id].to_i).first
 
-      File.binwite("../image/#{params[:id]}.#{params[:ext]}", post[:imgdata])
+      File.binwrite("../image/#{params[:id]}.#{params[:ext]}", post[:imgdata])
 
       if (params[:ext] == "jpg" && post[:mime] == "image/jpeg") ||
           (params[:ext] == "png" && post[:mime] == "image/png") ||

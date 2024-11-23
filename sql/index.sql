b868383b26c74b7c3af6eccfd083d54d58b055ec
yoshiyoshiharu <haruki.osaka.u@gmail.com>
2024-11-23 15:46:30 +0900

    追加したindex

--- sql/index.sql
+++ sql/index.sql
@@ -3,3 +3,5 @@ CREATE INDEX index_comments_post_id_created_at ON comments (post_id, created_at)
 CREATE INDEX index_comments_user_id ON comments (user_id);
 CREATE INDEX index_posts_user_id_created_at ON posts (user_id, created_at);
 CREATE INDEX index_posts_created_at ON posts (created_at);
+CREATE INDEX index_posts_created_at_desc ON posts (created_at DESC);
+CREATE INDEX index_del_flg ON users (del_flg);

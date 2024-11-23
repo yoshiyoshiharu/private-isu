7050f687d63ff9851164a1cf1b5071404b00feed
Haruki Yoshida <haruki.osaka.u@gmail.com>
2024-11-23 15:45:55 +0900

    Fix n1 make posts users (47104) (#28)
    * fix
    
    * fix
    
    * fix
    
    * use makeposts
    
    * 他にも適用
    
    * result

--- ruby/app.rb
+++ ruby/app.rb
@@ -115,14 +115,15 @@ module Isuconp
               comment[:user_id]
             ).first
           end
+
           post[:comments] = comments.reverse
 
-          post[:user] = db.prepare('SELECT * FROM `users` WHERE `id` = ?').execute(
-            post[:user_id]
-          ).first
+          post[:user] = {
+            id: post[:user_id],
+            account_name: post[:account_name]
+          }
 
-          posts.push(post) if post[:user][:del_flg] == 0
-          break if posts.length >= POSTS_PER_PAGE
+          posts.push(post)
         end
 
         posts
@@ -222,7 +223,15 @@ module Isuconp
     get '/' do
       me = get_session_user()
 
-      results = db.query('SELECT `id`, `user_id`, `body`, `created_at`, `mime` FROM `posts` ORDER BY `created_at` DESC LIMIT 20')
+      results = db.query("
+        SELECT p.`id`, p.`user_id`, p.`body`, p.`created_at`, p.`mime`, u.`account_name`
+        FROM `posts` as p
+        INNER JOIN `users` as u ON p.user_id = u.id
+        WHERE u.`del_flg` = 0
+        ORDER BY `created_at` DESC
+        LIMIT 20
+        ")
+
       posts = make_posts(results)
 
       erb :index, layout: :layout, locals: { posts: posts, me: me }
@@ -237,7 +246,14 @@ module Isuconp
         return 404
       end
 
-      results = db.prepare('SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` WHERE `user_id` = ? ORDER BY `created_at` DESC').execute(
+      results = db.prepare("
+        SELECT p.`id`, p.`user_id`, p.`body`, p.`created_at`, p.`mime`, u.`account_name`
+        FROM `posts` as p
+        INNER JOIN `users` as u ON p.user_id = u.id
+        WHERE u.`del_flg` = 0 AND u.`id` = ?
+        ORDER BY `created_at` DESC
+        LIMIT 20
+      ").execute(
         user[:id]
       )
       posts = make_posts(results)
@@ -266,7 +282,14 @@ module Isuconp
 
     get '/posts' do
       max_created_at = params['max_created_at']
-      results = db.prepare('SELECT `id`, `user_id`, `body`, `mime`, `created_at` FROM `posts` WHERE `created_at` <= ? ORDER BY `created_at` DESC LIMIT 20').execute(
+      results = db.prepare(
+        "SELECT p.`id`, p.`user_id`, p.`body`, p.`created_at`, p.`mime`, u.`account_name`
+        FROM `posts` as p
+        INNER JOIN `users` as u ON p.user_id = u.id
+        WHERE u.`del_flg` = 0 AND p.`created_at` <= ?
+        ORDER BY `created_at` DESC
+        LIMIT 20 "
+      ).execute(
         max_created_at.nil? ? nil : Time.iso8601(max_created_at).localtime
       )
       posts = make_posts(results)
@@ -275,7 +298,12 @@ module Isuconp
     end
 
     get '/posts/:id' do
-      results = db.prepare('SELECT * FROM `posts` WHERE `id` = ?').execute(
+      results = db.prepare(
+        "SELECT p.`id`, p.`user_id`, p.`body`, p.`created_at`, p.`mime`, u.`account_name`
+        FROM `posts` as p
+        INNER JOIN `users` as u ON p.user_id = u.id
+        WHERE u.`del_flg` = 0 AND p.`id` = ?"
+      ).execute(
         params[:id]
       )
       posts = make_posts(results, all_comments: true)
